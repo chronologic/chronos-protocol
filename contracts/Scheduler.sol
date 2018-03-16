a34f913c700096d40d0ebcd2b46ae991736043f5
lsaether <Lsaether@protonmail.com>
2018-03-16 16:44:43 +0100

    init

---
+++ contracts/Scheduler.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.4.20;
+
+// import IPFS.sol
+
+contract Scheduler {
+    function () public { revert(); }
+
+    function schedule(string _serializedParams) 
+        public payable returns (address scheduledTx)
+    {
+        // Deploy the ScheduledTransaction contract
+        address recipient;
+        uint256 value;
+        uint256 callGas;
+        uint256 gasPrice;
+        uint256 executionWindowStart;
+        uint256 executionWindowLength;
+        uint256 bounty;
+        uint256 fee;
+        // No requiredDeposit - Use Day Token now
+
+        assembly {
+            recipient := mload(_serializedParams)
+            value := mload(add(_serializedParams, 32))
+            callGas := mload(add(_serializedParams, 64))
+            gasPrice := mload(add(_serializedParams, 96))
+            executionWindowStart := mload(add(_serializedParams, 128))
+            executionWindowLength := mload(add(_serializedParams, 160))
+            bounty := mload(add(_serializedParams, 192))
+            fee := mload(add(_serializedParams, 224))
+            // CallData = everything after this
+        }
+
+        uint endowment = value + callGas * gasPrice + bounty + fee;
+        require(msg.value >= endowment);
+
+        // bytes hash = IPFS.generateHash(_serializedParams);
+        scheduledTx = createTransaction(hash)
+        require(scheduledTx != 0x0);
+
+        ScheduledTransaction(scheduledTx).init.value(msg.value)
+        // Store in the request tracker
+        NewScheduledTransaction(scheduledTx, msg.sender)
+    }
+
+    function createTransaction() {}
+
+    event NewScheduledTransaction(address _tx, address indexed _creator);
+}
