c814d734607c381d56fca531376ccf64d15f9950
lsaether <Lsaether@protonmail.com>
2018-03-19 14:48:29 +0100

    ipfs wrappers

--- scripts/ipfsStore.js
+++ scripts/ipfsStore.js
@@ -1,22 +1,60 @@
 const IPFS = require('ipfs')
-const node = new IPFS()
 
-node.once('ready', () => {
-    console.log('IPFS is now ON')
-    node.id((err, res) => {
-        if (!err) {
-            console.log(`Node ID: ${res.id}`)
-        }
-    })
-
-    node.files.add([Buffer.from('randomString')], (err, filesAdded) => {
-        if (err) throw err
-        const hash = filesAdded[0].hash
-        console.log(`Hash: ${hash}`)
-
-        node.files.cat(hash, (err, data) => {
-            if (err) throw err
-            console.log(`Data: ${data}`)
+const startNode = () => {
+    const node = new IPFS()
+    return new Promise(resolve => {
+        node.once('ready', () => {
+            resolve(node)
         })
     })
-})
+}
+
+const getId = (node) => {
+    return new Promise(resolve => {
+        node.id((err, res) => {
+            if (err) throw err
+            resolve(res.id)
+        })
+    })
+}
+
+const addString = (node, msg) => {
+    return new Promise(resolve => {
+        node.files.add([Buffer.from(msg)], (err, filesAdded) => {
+            if (err) throw err
+            resolve(filesAdded[0].hash)
+        })
+    })
+}
+
+const getString = (node, hash) => {
+    return new Promise(resolve => {
+        node.files.cat(hash, (err, data) => {
+            if (err) throw err
+            resolve(data)
+        })
+    })
+}
+
+const shutdown = (node) => {
+    return new Promise(resolve => {
+        node.stop(() => {
+            resolve(1)
+        })
+    })
+}
+
+const main = async () => {
+    const n = await startNode()
+    console.log('node started')
+    const id = await getId(n)
+    console.log('node id: ', id)
+    const hash = await addString(n, 'datadadatadata')
+    console.log('msg added: ', hash)
+    const msg = await getString(n, hash)
+    console.log('msg retrieved: ', msg.toString())
+    await shutdown(n)
+    console.log('shutdown node')
+}
+
+main()
