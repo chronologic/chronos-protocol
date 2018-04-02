ba69e1ef67fee31ce8f0345adfc2404deb832d2f
lsaether <Lsaether@protonmail.com>
2018-04-02 18:56:06 +0200

    cleanScheduler.sol

--- contracts/Scheduler.sol
+++ contracts/Scheduler.sol
@@ -28,50 +28,19 @@ contract Scheduler is CloneFactory {
     function schedule(bytes _serializedTransaction) 
         public payable returns (address scheduledTx)
     {
-        // bytes2 temporalUnit;
-        // address recipient;
         uint256 value;
         uint256 callGas;
         uint256 gasPrice;
-        // uint256 executionWindowStart;
-        // uint256 executionWindowLength;
         uint256 bounty;
         uint256 fee;
 
-        // uint256 callDataLen;
-        // uint256 callDataLoc;
-        // No requiredDeposit - Use Day Token now
-
         assembly {
-            // temporalUnit := mload(add(_serializedTransaction, 32))
-            // recipient := mload(add(_serializedTransaction, 34))
             value := mload(add(_serializedTransaction, 66))
             callGas := mload(add(_serializedTransaction, 98))
             gasPrice := mload(add(_serializedTransaction, 130))
-            // executionWindowStart := mload(add(_serializedTransaction, 162))
-            // executionWindowLength := mload(add(_serializedTransaction, 194))
             bounty := mload(add(_serializedTransaction, 226))
             fee := mload(add(_serializedTransaction, 258))
-            // CallData = everything after this
-            // first 32 bytes of array header
-            // callDataLen := mload(add(_serializedTransaction, 322)) // first 32 bytes is length
-            // callDataLoc := add(_serializedTransaction, 354) // the location of callData
         }
-
-        // bytes memory callData = toBytes(callDataLoc, callDataLen);
-
-        // EventEmitter(eventEmitter).logParameters(
-        //     temporalUnit,
-        //     recipient,
-        //     value,
-        //     callGas,
-        //     gasPrice,
-        //     executionWindowStart,
-        //     executionWindowLength,
-        //     bounty,
-        //     fee,
-        //     callData
-        // );
 
         uint endowment = value + callGas * gasPrice + bounty + fee;
         require(msg.value >= endowment);
