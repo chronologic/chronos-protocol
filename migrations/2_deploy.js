e17b992f2866e1ee14ed864265d0c2d7e27dce0f
lsaether <Lsaether@protonmail.com>
2018-04-09 18:05:16 +0200

    add dependencies

---
+++ migrations/2_deploy.js
@@ -0,0 +1,7 @@
+const ClaimingPool = artifacts.require('ClaimingPool.sol')
+const CloneFactory = artifacts.require('CloneFactory.sol')
+const EventEmitter = artifacts.require('EventEmitter.sol')
+const IPFS = artifacts.require('IPFS.sol')
+const ScheduledTransaction = artifacts.require('ScheduledTransaction.sol')
+const Scheduler = artifacts.require('Scheduler.sol')
+
