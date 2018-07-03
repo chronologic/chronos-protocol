1d8d4ceef2e8d75afa62b8e956159f0f523d5385
Lsaether <Lsaether@protonmail.com>
2018-07-03 21:47:51 +0300

    some reorg

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,28 +1,40 @@
 const fs = require('fs');
 
+// 0 - Chronos Offchain
+// 1 - Chronos Onchain
+const DEPLOY_TYPE = 0;
+
+// Chronos Offchain
+const c1 = artifacts.require('C.sol');
+
+// Chronos Onchain
 const EventEmitter = artifacts.require('EventEmitter.sol');
 const ParseLib = artifacts.require('ParseLib.sol');
 const ScheduledTransaction = artifacts.require('ScheduledTransaction.sol');
 const Scheduler = artifacts.require('Scheduler.sol');
 
 module.exports = (deployer) => {
-  deployer.deploy(ParseLib)
-  .then(() => {
-    deployer.link(ParseLib, ScheduledTransaction);
-    return deployer.deploy(ScheduledTransaction);
-  })
-  .then(() => {
-    return deployer.deploy(EventEmitter);
-  })
-  .then(() => {
-    return deployer.deploy(Scheduler, EventEmitter.address, ScheduledTransaction.address);
-  })
-  .then(() => {
-    fs.writeFileSync('deployed.json', JSON.stringify({
-      eventEmitter: EventEmitter.address,
-      parseLib: ParseLib.address,
-      scheduledTransactionCore: ScheduledTransaction.address,
-      scheduler: Scheduler.address,
-    }));
-  })
+  if (DEPLOY_TYPE === 0) {
+    deployer.deploy(c1);
+  } else {
+    deployer.deploy(ParseLib)
+    .then(() => {
+      deployer.link(ParseLib, ScheduledTransaction);
+      return deployer.deploy(ScheduledTransaction);
+    })
+    .then(() => {
+      return deployer.deploy(EventEmitter);
+    })
+    .then(() => {
+      return deployer.deploy(Scheduler, EventEmitter.address, ScheduledTransaction.address);
+    })
+    .then(() => {
+      fs.writeFileSync('deployed.json', JSON.stringify({
+        eventEmitter: EventEmitter.address,
+        parseLib: ParseLib.address,
+        scheduledTransactionCore: ScheduledTransaction.address,
+        scheduler: Scheduler.address,
+      }));
+    })
+  }
 }
