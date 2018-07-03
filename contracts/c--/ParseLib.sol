edcd748ffbf435bb3cb7cffca052e7ff34de2aca
linux_user <linux@example.com>
2018-07-03 16:48:52 +0300

    c--

---
+++ contracts/c--/ParseLib.sol
@@ -0,0 +1,138 @@
+pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
+
+library ParseLib {
+    struct Transaction {
+        address to;
+        uint256 gas;
+        uint256 gasPrice;
+        uint256 value;
+
+        uint256 temporalUnit;
+        uint256 executionWindowStart;
+        uint256 executionWindowLength;
+
+        uint256 bounty;
+
+        bytes data;
+    }
+
+    function inExecutionWindow(Transaction _T)
+        public view returns (bool)
+    {
+        if (_T.temporalUnit == 1) {
+            return (
+                block.number >= _T.executionWindowStart &&
+                block.number < _T.executionWindowStart + _T.executionWindowLength
+            );
+        } else if (_T.temporalUnit == 2) {
+            return (
+                block.timestamp >= _T.executionWindowStart &&
+                block.timestamp < _T.executionWindowStart + _T.executionWindowLength
+            );
+        } else { return false; }
+    }
+
+    function execute(Transaction _T) 
+        public returns (bool)
+    {
+        require(_T.inExecutionWindow());
+        require(_T.gasPrice == tx.gasprice);
+
+    }
+
+    function fromData(bytes _serializedTransaction)
+        public returns (Transaction)
+    {
+        address to;
+        uint256 callGas;
+        uint256 gasPrice;
+        uint256 value;
+        uint256 temporalUnit;
+        uint256 executionWindowStart;
+        uint256 executionWindowLength;
+        uint256 bounty;
+
+        assembly {
+            to := mload(add(_serializedTransaction, 32))
+            callGas := mload(add(_serializedTransaction, 64))
+            gasPrice := mload(add(_serializedTransaction, 96))
+            value := mload(add(_serializedTransaction, 128))
+            temporalUnit := mload(add(_serializedTransaction, 160))
+            executionWindowStart := mload(add(_serializedTransaction, 192))
+            executionWindowLength := mload(add(_serializedTransaction, 224))
+            bounty := mload(add(_serializedTransaction, 256))
+        }
+
+        bytes memory data = getBytesAtLocation(_serializedTransaction, 352);
+
+        return Transaction(
+            to,
+            callGas,
+            gasPrice,
+            value,
+            temporalUnit,
+            executionWindowStart,
+            executionWindowLength,
+            bounty,
+            data
+        );
+    }
+
+    function getBytesAtLocation(bytes _data, uint256 _location)
+        public pure returns (bytes)
+    {
+        uint256 start;
+        assembly {
+            start := mload(add(_data), _location)
+        }
+
+        // First get the locations based on `start` position.
+        uint256 len = start + 0x20;
+        uint256 ret = len + 0x20;
+
+        assembly {
+            // Overwrite the values onto the locations.
+            len := mload(add(_data, len))
+            data := add(_data, data)
+        }
+
+        return toBytes(data, len);
+    } 
+
+    function toBytes(uint256 _ptr, uint256 _len)
+        public pure returns (bytes)
+    {
+        bytes memory ret = new bytes(_len);
+
+        uint256 retptr;
+
+        assembly {
+            retptr := add(ret, 0x20)
+        }
+
+        memcpy(retptr, _ptr, _len);
+        return ret;
+    }
+
+    function memcpy(uint256 _dest, uint256 _src, uint256 _len)
+        public pure
+    {
+        // Copy word-length chunks while possible
+        for (; _len >= 0x20; len -= 0x20) {
+            assembly {
+                mstore(_dest, mload(_src))
+            }
+            _dest += 0x20;
+            _src += 0x20;
+        }
+
+        // Copy remaining bytes
+        uint256 mask = 0x100 ** (0x20 - _len) - 1;
+        assembly {
+            let srcpart := and(mload(_src), not(mask))
+            let destpart := and(mload(_dest), mask)
+            mstore(_dest, or(destpart, srcpart))
+        }
+    }
+}
---
+++ contracts/c--/ScheduledTransaction.sol
@@ -0,0 +1,100 @@
+pragma solidity ^0.4.24;
+
+import "./ParseLib.sol";
+
+contract ScheduledTransaction {
+    bytes32 public dataHash;
+
+    enum State {
+        Null,
+        Initialized,
+        Executed,
+        Cancelled
+    }
+
+    State public state = State.Null;
+
+    struct Transaction {
+
+    }
+
+    /**
+     * MODIFIER
+     */
+
+    modifier requireCorrectData(bytes _serializedTransaction) {
+        bytes32 _hash = keccak256(_serializedTransaction);
+        require(dataHash == _hash);
+        _;
+    }
+
+    modifier requireInState(State _state) {
+        require(state == _state);
+        _;
+    }
+
+    /**
+     * FUNCTION
+     */
+
+    function initialize(bytes32 _dataHash)
+        requireInState(State.Null)
+        public payable
+    {
+        dataHash = _dataHash;
+
+        state = State.Initialized;
+    }
+
+    function execute(bytes _serializedTransaction)
+        requireCorrectData(_serializedTransaction)
+        requireInState(State.Initialized)
+        public returns (bool)
+    {
+        ParseLib.Transaction tx_ = ParseLib.fromData(_serializedTransaction);
+
+        require(gasleft() >= tx_.callGas + 180000 - 25000);
+
+        require(tx_.inExecutionWindow());
+        require(tx_.gasPrice == tx.gasprice);
+
+        state = State.Executed;
+        bool success = tx_.execute();
+
+        tx_.payBounty();
+
+        // Transfer the rest to the owner
+
+        return true;
+    }
+
+    function cancel(bytes _serializedTransaction) 
+        requireInState(State.Initialized)
+        public returns (bool)
+    {
+        // Check Owner
+        require(msg.sender == getOwner(_serializedTransaction));
+
+        state = State.Cancelled;
+        owner.transfer(address(this).balance);
+        return true;
+    }
+
+    function proxy(address _to, bytes _data, bytes _serializedTransaction)
+        requireInState(State.Executed)
+        public payable returns (bool)
+    {
+        require(msg.sender = getOwner(_serializedTransaction));
+        return _to.call.value(msg.value)(_data);
+    }
+
+    function getOwner(bytes _serializedTransaction)
+        requireCorrectData(_serializedTransaction)
+        public pure returns (address owner)
+    {
+        assembly {
+            owner := mload(add(_serializedTransaction), 000)
+        }
+    }
+
+}
